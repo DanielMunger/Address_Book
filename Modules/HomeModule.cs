2134ee9930cedd9fd247f82ba7b08ac9c204ed57
Daniel Munger <mungerda@gmail.com>
2016-11-18 09:02:33 -0800

    create 'get' and 'post' methods

--- Modules/HomeModule.cs
+++ Modules/HomeModule.cs
@@ -8,8 +8,22 @@ namespace AddressBookS //replace with your desired namespace
   {
     public HomeModule()
     {
-      Get["/"] = _ => View["index.cshtml"];
-      //Insert your GETs and POSTs here
+      Get["/"] = _ => {
+        List<Contact> allContacts = Contact.GetAll();
+        View["index.cshtml", allContacts];
+      };
+
+      Get["contact/new"] = _ => View["/contact_new_form.cshtml"];
+
+      Post["/contact"] = _ => {
+        string newName = Request.Form["new_contact_name"];
+        string newPhoneNumber = Request.Form["new_phone_number"];
+        string newEmail = Request.Form["new_email"];
+        string newAddress = Request.Form["new_address"];
+        Contact newContact = new Contact(newName, newPhoneNumber, newEmail, newAddress);
+
+        return View["new_contact.cshtml"];
+      }
     }
   }
 }
